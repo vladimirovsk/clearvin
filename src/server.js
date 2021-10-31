31bcf0cc092b3cfffa1232ac7b8b845bbcd6a864
Сергей Владимиров <vladimirovsk@gmail.com>
2021-10-31 12:35:32 +0100

    remove old files

--- src/server.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var express_1 = require("express");
-var app = (0, express_1["default"])();
-app.get('/', function (req, res) {
-    res.send('Hello world!');
-});
-app.listen(5000);
